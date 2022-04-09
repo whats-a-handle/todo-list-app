11c40d46ce697b04123c261f53dbd95e69930bcb
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2022-04-09 00:24:59 -0700

    Add to list of todoitems (no key per item yet), super basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import ToDoList from './Components/ToDoList'
 import ToDoListItem from './Components/TodoListItem'
 import CreateItemForm from './Components/CreateItemForm'
 import {Container,Grid} from '@mui/material';
-export default function App() {
-  let myItems = [<ToDoListItem itemName='item 1' itemBody='heres my body'/>,
-  <ToDoListItem itemName='item 2' /*no secondary*/ />]
 
+
+
+export default function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const createToDoItem = (itemName)=>{
+    const newToDo = (<ToDoListItem itemName={itemName}/>);
+    setTodoItems((todoItems) => [...todoItems,newToDo]);
+  }
   return (
       <Container style={{backgroundColor:'red'}}>
         <Grid container style={{backgroundColor:'blue'}}>
           <Grid item xs={12}>
-            <CreateItemForm />
+            <CreateItemForm createToDoItem={createToDoItem}/>
           </Grid>
           <Grid item xs={12}>
             <Grid container>
               <Grid item xs={2}/>
               <Grid item xs={8}>
-                <ToDoList > {myItems} </ToDoList>
+                <ToDoList > {todoItems} </ToDoList>
               </Grid>
               <Grid item xs={2}/>
             </Grid>
--- frontend/src/Components/CreateItemForm.js
+++ frontend/src/Components/CreateItemForm.js
@@ -1,8 +1,15 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button,Container,Grid,TextField} from '@mui/material';
 
 
 export default function CreateItemForm(props){
+    const [newToDoName,setNewToDoName] = useState('');
+
+    const handleChange = (event) =>{
+        setNewToDoName(event.target.value);
+    }
+
+
  return(
     <Container style={{backgroundColor:'yellow'}}>
         <Grid container>
@@ -10,8 +17,8 @@ export default function CreateItemForm(props){
                 <Grid container>
                     <Grid item xs={3}/>
                     <Grid item xs={6} align="center">
-                        <TextField style={{width:'100%'}} id="standard-basic" label="New to-do " variant="standard" />
-                        <Button variant='outlined'>Add</Button>
+                        <TextField value={newToDoName} style={{width:'100%'}} id="standard-basic" label="New to-do " variant="standard" onChange={(event)=>{handleChange(event)}} />
+                        <Button variant='outlined' onClick={()=>{props.createToDoItem(newToDoName); setNewToDoName('')}}>Add</Button>
                     </Grid>
                 <Grid item xs={3}/>
                 </Grid>
