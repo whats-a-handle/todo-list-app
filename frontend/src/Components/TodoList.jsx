bc03f12c66be76238bab5e77dfdb4c26e03f7b90
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2022-04-21 00:41:43 -0700

    Added back todolist to fix cache issue on heroku

---
+++ frontend/src/Components/TodoList.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Grid } from '@mui/material';
+import TodoCard from './TodoCard';
+
+export default function TodoList() {
+  const items = [<TodoCard title="title 1" isCompleted />,
+    <TodoCard title="title 2" isCompleted />, <TodoCard title="title 3" isCompleted />, <TodoCard title="title 4" isCompleted />];
+  return (
+    <Grid container item xs={12} spacing={2} style={{ padding: 20 }} align="center">
+      {items.map((item) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={3}>
+          {item}
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
