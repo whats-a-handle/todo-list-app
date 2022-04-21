89e45a757bd31fceefc3692d2b474f4fe9ba41d8
whats-a-handle <24727927+whats-a-handle@users.noreply.github.com>
2022-04-21 12:34:52 -0700

    Moved column names inside columns and added more styling to columns

--- frontend/src/Components/TodoList.jsx
+++ frontend/src/Components/TodoList.jsx
@@ -7,39 +7,61 @@ export default function TodoList() {
     <TodoCard title="title 2" isCompleted />, <TodoCard title="title 3" isCompleted />, <TodoCard title="title 4" isCompleted />];
   return (
     <Box sx={{ maxHeight: '100%' }}>
-      <Grid container item xs={12} align="center" justifyContent="center">
-        <Grid item xs={3}>
-          <Typography color="inherit">
-            Todo
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Typography>
-            In-progress
-          </Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Typography>
-            Done!
-          </Typography>
-        </Grid>
-      </Grid>
       <Grid container item xs={12} style={{ padding: '20px' }} align="center" justifyContent="center">
-        <Grid container item xs={3} rowSpacing={2} sx={{ border: '1px solid white', paddingBottom: '10px', marginRight: 3 }}>
+        <Grid
+          container
+          item
+          xs={3}
+          rowSpacing={2}
+          sx={{
+            borderRadius: 3, border: '1px solid rgb(246, 247, 248)', backgroundColor: 'rgb(246, 247, 248)', paddingBottom: '10px', marginRight: 3, boxShadow: 1,
+          }}
+        >
+          <Grid container item xs={12} align="left" sx={{ paddingLeft: 4 }}>
+            <Typography>
+              Todo
+            </Typography>
+          </Grid>
           {items.map((item) => (
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
               {item}
             </Grid>
           ))}
         </Grid>
-        <Grid container item xs={3} rowSpacing={2} sx={{ border: '1px solid white', paddingBottom: '10px', marginRight: 3 }}>
+        <Grid
+          container
+          item
+          xs={3}
+          rowSpacing={2}
+          sx={{
+            borderRadius: 3, border: '1px solid white', backgroundColor: 'rgb(246, 247, 248)', paddingBottom: '10px', marginRight: 3, boxShadow: 1,
+          }}
+        >
+          <Grid container item xs={12} align="left" sx={{ paddingLeft: 4 }}>
+            <Typography>
+              In-progress
+            </Typography>
+          </Grid>
           {items.map((item) => (
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
               {item}
             </Grid>
           ))}
         </Grid>
-        <Grid container item xs={3} rowSpacing={2} sx={{ border: '1px solid white', paddingBottom: '10px' }}>
+        <Grid
+          container
+          item
+          xs={3}
+          rowSpacing={2}
+          sx={{
+            borderRadius: 3, border: '1px solid white', backgroundColor: 'rgb(246, 247, 248)', paddingBottom: '10px', boxShadow: 1,
+          }}
+        >
+          <Grid container item xs={12} align="left" sx={{ paddingLeft: 4 }}>
+            <Typography>
+              Done!
+            </Typography>
+          </Grid>
           {items.map((item) => (
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
               {item}
